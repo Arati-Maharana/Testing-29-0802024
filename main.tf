3cb1eb973391024e9644c09fbe93e78348ed6f26
Arati-Maharana <aratimaharana95@gmail.com>
2024-08-29 22:53:08 +0530

    practice code added

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rgblock" {
   name= "aratirg"
   location = "west us"
 }
+resource "azurerm_resource_group" "rgblock2" {
+  name= "aratirg2"
+  location = "west us"
+}

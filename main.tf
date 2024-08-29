672bd94ae7c6b741a69dd82764e117bb5b5a0c6e
shaheel03 <meetjai086@gmail.com>
2024-08-29 23:04:04 +0530

    added new rg by jagdamba

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "rgblock2" {
   name= "aratirg2"
   location = "west us"
 }
+
+resource "azurerm_resource_group" "rgblock3" {
+  name= "aratirg3"
+  location = "west us"
+}

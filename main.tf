41d4335bf5cc2c4c6a46a57f044492572ba22aba
Arati-Maharana <aratimaharana95@gmail.com>
2024-08-29 23:22:52 +0530

    pull request

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-resource "azurerm_resource_group" "rgblock" {
-  name= "aratirg"
-  location = "west us"
-}
-resource "azurerm_resource_group" "rgblock2" {
-  name= "aratirg2"
-  location = "west us"
-}
 
 resource "azurerm_resource_group" "rgblock3" {
   name= "aratirg3"
